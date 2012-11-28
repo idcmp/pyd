46bb81723e31acd5f6db936a6784140bff64b01e
JAmes Atwill <jatwill@linuxstuff.org>
2012-11-28 11:09:21 -0800

    Updates for daily reporting
    - move "get yesterday" logic into the toolbox
    - make dscrum use toolboxed get yesterday
    - create "didw" to generate output for the IDW report.

--- pyd/tools/toolbox.py
+++ pyd/tools/toolbox.py
@@ -7,11 +7,56 @@ from datetime import date
 from pyd.api import diaryreader as reader
 from pyd.api import diarywriter as writer
 from pyd.api import diarymodel as model
+from pyd.api import naming
+from pyd.api import carryforward as cf
+from pyd.api.diarymodel import MAXIMUM_HOLIDAY_WEEKS
+
 
 def find_todos_in_file(filename):
     dr = reader.DiaryReader()
     week = dr.read_file(filename)
     return model.find_todos_in_week(week)
+
+
+def find_yesterday():
+    """Find the last day in the diary which is not today;
+     this will go back up to MAXIMUM_HOLIDAY_WEEKS if needed."""
+    current_diary = naming.current_name()
+    cf.perform_carryforward()
+    ensure_current_header_exists(current_diary)
+
+    days_to_go_back = 0
+    yesterday = None
+
+    while yesterday is None:
+        days_to_go_back += 1
+        yesterday = _try_yesterday(days_to_go_back)
+        if days_to_go_back == MAXIMUM_HOLIDAY_WEEKS * 7:
+            return None
+    return yesterday
+
+
+def _try_yesterday(days_to_go_back):
+    """Private."""
+    yesterday_diaryname = naming.relative_name(days_ago=days_to_go_back)
+
+    week = reader.DiaryReader().read_file(yesterday_diaryname)
+
+    if yesterday_diaryname == naming.current_name():
+        if len(week.days()) == 0 or is_today(week.days()[0]):
+            return None
+
+        # Pair up N and N+1 in tuples, iterate through each tuple and if N+1 matches
+        # today, then return N.
+        for yesterday, today in zip(week.days(), week.days()[1:]):
+            if is_today(today):
+                return yesterday
+
+    size = len(week.days())
+    if not size:
+        return None
+
+    return week.days()[size - 1]
 
 
 def ensure_current_header_exists(filename):
